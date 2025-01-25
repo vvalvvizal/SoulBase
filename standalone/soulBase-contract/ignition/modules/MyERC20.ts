bb53cfb4a6f958c2d657467872ef4ff322f25ff6
vvalvvizal <vvalvvizal@gmail.com>
2025-01-25 12:43:37 +0900

    Initial commit

---
+++ standalone/soulBase-contract/ignition/modules/MyERC20.ts
@@ -0,0 +1,12 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const MyERC20Module = buildModule("MyERC20Module", (m) => {
+  const MyERC20 = m.contract("MyERC20", []);
+
+  return { MyERC20 };
+});
+
+export default MyERC20Module;
