f4d90cf9cca3ade9b1ea00d4d83aa871c6dbf2af
vvalvvizal <vvalvvizal@gmail.com>
2025-01-28 00:04:11 +0900

    use openzeppelin ERC20

--- standalone/soulBase-contract/contracts/MyERC20.sol
+++ standalone/soulBase-contract/contracts/MyERC20.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract MyERC20{
-     function mint() public payable{
+contract MyERC20 is ERC20{
+
+
+    constructor( ) ERC20("My ERC20 Token", "MyERC20"){
 
     }
     
--- standalone/soulBase-contract/hardhat.config.ts
+++ standalone/soulBase-contract/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
+require('dotenv').config();
+
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
+  defaultNetwork: 'amoy',
+  networks: {
+    amoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
 };
 
 export default config;
