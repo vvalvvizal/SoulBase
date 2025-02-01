1abb62ef447a8581f958145c6f8c6126c44f25e9
vvalvvizal <vvalvvizal@gmail.com>
2025-02-01 23:52:44 +0900

    create MyERC20Router

--- standalone/soulBase-contract/contracts/MyERC20.sol
+++ standalone/soulBase-contract/contracts/MyERC20.sol
@@ -7,9 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20 is ERC20{
 
+    address public owner;
+    uint public MAX_SUPPLY;//토큰 발행량 
 
     constructor( ) ERC20("My ERC20 Token", "MyERC20"){
+        MAX_SUPPLY = 1000000 * 10**decimals();
+        _mint(address(this), MAX_SUPPLY );//배포 시에 MAX_SUPPLY 만큼 발행
+        owner = msg.sender;
+    }
 
+    function mint(address account, uint256 amount) external{
+        _mint(account, amount);
     }
     
 }
